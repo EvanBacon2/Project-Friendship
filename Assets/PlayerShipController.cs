63c6d4cc9419df17674e765f2f1f315c3a0e92e5
Evan Bacon <31549754+EvanBacon2@users.noreply.github.com>
2022-07-09 01:29:30 -0700

    Fixed jittering issues caused when moving and rotating playerShip.

--- Assets/PlayerShipController.cs
+++ Assets/PlayerShipController.cs
@@ -4,29 +4,33 @@ using UnityEngine;
 
 public class PlayerShipController : MonoBehaviour {
     private Rigidbody selfRigidBody;
-
     private float speed;
 
     // Start is called before the first frame update
     void Start() {
         selfRigidBody = GetComponent<Rigidbody>();
-        speed = 1500;
+        speed = 100;
     }
 
     // Update is called once per frame
     void Update() {
-        
+        rotateToMouse();
     }
 
 	private void FixedUpdate() {
-        rotateToMouse();
         moveShip();
 	}
 
 	private void rotateToMouse() {
-        Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        /*Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         float turnAngle = Mathf.Atan2(mouse_pos.y - transform.position.y, mouse_pos.x - transform.position.x) * Mathf.Rad2Deg;
         Quaternion lookRotation = Quaternion.AngleAxis(turnAngle - 90, new Vector3(0,0,70));
+        transform.rotation = lookRotation;*/
+
+        Vector3 mouse_pos = Input.mousePosition;
+        float turnAngle = Mathf.Atan2(Screen.height / 2 - mouse_pos.y, 
+                                      Screen.width / 2 - mouse_pos.x) * Mathf.Rad2Deg;
+        Quaternion lookRotation = Quaternion.AngleAxis(turnAngle - 90, new Vector3(0, 0, 70));
         transform.rotation = lookRotation;
     }
 
@@ -34,7 +38,8 @@ public class PlayerShipController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        if (selfRigidBody.velocity.magnitude < 2000)
-            selfRigidBody.AddForce(movement * speed);
+        selfRigidBody.AddForce(movement * speed);
+        if (selfRigidBody.velocity.magnitude > 120)
+            selfRigidBody.velocity = selfRigidBody.velocity.normalized * 120;
     }
 }
